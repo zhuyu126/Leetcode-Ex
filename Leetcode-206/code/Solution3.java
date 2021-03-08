3c6aad5e9eb8a0320e87865c1460d5af67f90b5a
zhuyu <zhuyu553@gmail.com>
2021-03-08 15:04:10 +0800

    双指针迭代解法

---
+++ Leetcode-206/code/Solution3.java
@@ -0,0 +1,13 @@
+public class Solution3 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        while (curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+}
